633283e1b5ddd2741d75814e541a53bed249400d
ManuelTristanMartinRuiz <manueltristanmartin@gmail.com>
2024-02-06 11:37:43 +0100

    Update pruebagit.R

--- pruebagit.R
+++ pruebagit.R
@@ -1,2 +1,7 @@
 nuevo_dir <- "C:/Users/manueltristan/Documents/clase1"
 setwd(nuevo_dir)
+if (file.exists(nuevo_dir)) {cat("Directorio creado correctamente: ", nuevo_dir, "\n")
+} else {
+    cat("Fallo al crear directorio: ", nuevo_dir, "\n")
+  }
+  
